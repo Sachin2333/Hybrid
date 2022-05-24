889721bd02ccdcce444d18953b5909294724cce5
Sachin2333 <sachingodase5130@gmail.com>
2022-05-24 13:23:59 +0530

    first commit

---
+++ src/main/java/com/mindtree/TestCases/TC_LoginPage.java
@@ -0,0 +1,24 @@
+package com.mindtree.TestCases;
+
+import org.testng.annotations.Test;
+
+import com.mindtree.PageObjects.LoginPage;
+
+public class TC_LoginPage extends BaseClass
+{
+	@Test
+	public void loginpage()
+	{
+		LoginPage lp=new LoginPage(driver);
+		log.info("Home page open successfully");
+		
+		lp.loginHover();
+		log.info("Login window hover successfully");
+		
+		lp.loginClk();
+		log.info("click on login btn successfully");
+		
+	}
+	
+
+}
